646ec56b69a0c2d28124c43a043ea32bcdd0aab0
Sanghyun Hong <sanghyun.hong@oregonstate.edu>
2026-03-08 23:27:34 -0700

    Update the rest scripts

--- megatron_preparation/pre_llama3_1.sh
+++ megatron_preparation/pre_llama3_1.sh
@@ -6,6 +6,10 @@ mkdir llama3-ckpts
 cd llama3-ckpts
 modelscope download --model LLM-Research/Meta-Llama-3.1-8B --local_dir ./llama3-ckpts/Meta-Llama-3.1-8B
 
+# set the Megatron pythonpath
+export TORCH_DISABLE_ADDR2LINE=1
+export PYTHONPATH=/workspace/FT-cutlass/Pai-Megatron-Patch/backends/megatron/Megatron-LM-250908:/workspace/FT-cutlass/Pai-Megatron-Patch:$PYTHONPATH
+
 # convert HF model to Megatron-Core model
 cd /workspace/FT-cutlass/Pai-Megatron-Patch/toolkits/model_checkpoints_convertor/llama
 bash hf2mcore_convertor_llama3_1.sh \
@@ -17,7 +21,9 @@ bash hf2mcore_convertor_llama3_1.sh \
 false \
 true \
 false \
-bf16
+bf16 \
+1 \
+1 2>&1 | tee /tmp/llama3_convert.log
 
 # make qwen 2.5 sft datasets
 cd /mnt
--- megatron_preparation/pre_qwen2_5.sh
+++ megatron_preparation/pre_qwen2_5.sh
@@ -4,6 +4,10 @@
 cd /mnt
 modelscope download --model Qwen/Qwen2.5-7B  --local_dir ./qwen-ckpts/Qwen2.5-7B
 
+# set the Megatron pythonpath
+export TORCH_DISABLE_ADDR2LINE=1
+export PYTHONPATH=/workspace/FT-cutlass/Pai-Megatron-Patch/backends/megatron/Megatron-LM-250908:/workspace/FT-cutlass/Pai-Megatron-Patch:$PYTHONPATH
+
 # convert HF model to Megatron-Core model
 cd /workspace/FT-cutlass/Pai-Megatron-Patch/toolkits/distributed_checkpoints_convertor
 bash scripts/qwen2_5/run_8xH20.sh \
@@ -12,7 +16,10 @@ bash scripts/qwen2_5/run_8xH20.sh \
 /mnt/qwen-ckpts/Qwen2.5-7B-to-mcore  \
 false \
 true \
-bf16
+bf16 \
+whatever \
+1 \
+1 2>&1 | tee /tmp/qwen2.5_convert.log
 
 # make qwen 2.5 sft datasets
 cd /workspace/FT-cutlass/Pai-Megatron-Patch/toolkits/sft_data_preprocessing
